0c8e930abbfdd00adf4f97d094b30e21c4f44080
VladimirVornikov <vornikov-vladimir@mail.ru>
2023-06-28 09:46:20 +0200

    28.06

--- Homeworks/src/main/java/Homework2/HomeworkEXTRA.java
+++ Homeworks/src/main/java/Homework2/HomeworkEXTRA.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class HomeworkEXTRA {
     public static void main(String[] args) {
-
-
-    }
+        Scanner myScannerInt = new Scanner(System.in);
+        int myInt;
+        System.out.println("Input number = ");
+            }
 }
